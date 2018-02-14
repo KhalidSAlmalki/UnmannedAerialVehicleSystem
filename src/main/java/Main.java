229a169ce3c8466e2fd6cca8d622bef3d4bd8320
KhalidSAlmalki <vb.95@hotmail.com>
2018-02-14 15:35:09 -0500

    FIRST PHASE implementing pipe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,66 @@
-public class Main {
+import java.io.*;
 
-    public Main(){
-        System.out.println("let us do it");
+public class Main {
+    static Process theProcess = null;
+
+    public static void main(String args[]) {
+
+
+        Runnable producer = () -> startProcess();
+
+        Runnable consumer = () -> readProcess();
+
+        new Thread(producer).start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread(consumer).start();
+
     }
-}
+    public static void startProcess(){
+
+        // call the CSAMP1 program
+        try
+        {
+            theProcess = Runtime.getRuntime().exec("java -Dname=khalidANaswesome -jar /Users/khalidalmalki/Documents/GitHub/UnmannedAerialVehicleSystem/out/artifacts/UnmannedAerialVehicleSystem_jar/UnmannedAerialVehicleSystem.jar");
+
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error on exec() method");
+            e.printStackTrace();
+        }
+
+    }
+
+    public  static void readProcess(){
+        BufferedReader inStream = null;
+
+        // read from the called program's standard output stream
+        try
+        {
+            inStream = new BufferedReader(new InputStreamReader
+                    (theProcess.getInputStream()));
+            while (inStream.readLine().length()!=-1){
+                System.out.println(inStream.readLine());
+
+            }
+
+
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error on inStream.readLine()");
+            e.printStackTrace();
+        }
+
+    }}
+
+
+
+
+
+
--- src/main/java/uas/navigation/Main.java
+++ src/main/java/uas/navigation/Main.java
@@ -2,11 +2,17 @@ package uas.navigation;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 /**
  * Created by Palash on 2/13/2018.
  */
 public class Main {
+    int counter = 0;
+
     Main() {
         JFrame frame = new JFrame();
         frame.setSize(1050, 200);
@@ -21,10 +27,35 @@ public class Main {
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        frame.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                counter = 50;
+                System.out.println("Closed");
+                e.getWindow().dispose();
+            }
+
+
+        });
+        while (counter <50){
+            label.setText("Navigation System: I AM TAKING YOU PLACES! "+ counter);
+            System.out.println(counter);
+            counter++;
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
     public static void main(String[] args) {
         new Main();
+
     }
 
 
